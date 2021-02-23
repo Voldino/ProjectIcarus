261d746977958ef71733cd5bd761edc5b9bd71a7
Zarozar <nathan25452002@gmail.com>
2021-02-24 00:25:04 +0700

    prototype for skilltree

---
+++ Assets/Scripts/GAMEPLAY/Cantina/CloseST.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloseST : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    bool ispressed = false;
+    public GameObject obj;
+
+    void Update()
+    {
+        if (ispressed)
+        {
+            obj.transform.Translate(-20f, 0, 0);
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ispressed = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        ispressed = false;
+    }
+}
