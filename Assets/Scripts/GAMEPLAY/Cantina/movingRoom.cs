c25e7e8f64df5bac18e299c134500fcaf71ab756
Zarozar <nathan25452002@gmail.com>
2021-02-24 20:52:43 +0700

    cantina/room update1

---
+++ Assets/Scripts/GAMEPLAY/Cantina/movingRoom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingRoom : MonoBehaviour
+{
+    public Transform t1;
+    public GameObject s1;
+    public Transform t2;
+    public GameObject s2;
+
+    public void moveToRoom() 
+    {
+        s1.transform.position = t1.transform.position;
+    }
+
+    public void moveToCantina()
+    {
+        t2.transform.position = s2.transform.position;
+    }
+
+}
