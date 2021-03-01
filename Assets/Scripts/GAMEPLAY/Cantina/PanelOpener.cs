19111622dc2037deb935dfc20d5beb854244514f
Zarozar <nathan25452002@gmail.com>
2021-03-02 04:26:45 +0700

    added buttons for skilltree panel

--- Assets/Scripts/GAMEPLAY/Cantina/PanelOpener.cs
+++ Assets/Scripts/GAMEPLAY/Cantina/PanelOpener.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PanelOpener : MonoBehaviour
 {
     public GameObject Panel;
+    public GameObject AtkUp;
+    public GameObject HpUp;
+    public GameObject SpeedUp;
 
     public void OpenPanel()
     {
@@ -15,5 +18,37 @@ public class PanelOpener : MonoBehaviour
             Panel.SetActive(!isActive);
         }
     }
+
+    public void OpenButton1()
+    {
+        if (AtkUp != null)
+        {
+            bool isActive = AtkUp.activeSelf;
+
+            AtkUp.SetActive(!isActive);
+        }
+    }
+
+    public void OpenButton2()
+    {
+        if (HpUp != null)
+        {
+            bool isActive = HpUp.activeSelf;
+
+            HpUp.SetActive(!isActive);
+        }
+    }
+
+    public void OpenButton3()
+    {
+        if (SpeedUp != null)
+        {
+            bool isActive = SpeedUp.activeSelf;
+
+            SpeedUp.SetActive(!isActive);
+        }
+    }
+
+
 
 }
