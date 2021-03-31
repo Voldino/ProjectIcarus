7465e03b5b66a6edc3e5c3e82d83483059cfc464
Zarozar <nathan25452002@gmail.com>
2021-04-01 01:16:13 +0700

    ST/WP updated

---
+++ Assets/Scripts/GAMEPLAY/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private float xMax;
+    [SerializeField]
+    private float yMax;
+    [SerializeField]
+    private float xMin;
+    [SerializeField]
+    private float yMin;
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, xMin, xMax), Mathf.Clamp(player.position.y, yMin, yMax), transform.position.z);
+    }
+}
