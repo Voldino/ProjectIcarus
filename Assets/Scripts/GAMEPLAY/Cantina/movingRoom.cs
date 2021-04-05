67644c376f6150dd41bd3604ed1b002a77f6f371
Zarozar <nathan25452002@gmail.com>
2021-04-05 20:51:14 +0700

    room tp + promt

--- Assets/Scripts/GAMEPLAY/Cantina/movingRoom.cs
+++ Assets/Scripts/GAMEPLAY/Cantina/movingRoom.cs
@@ -6,17 +6,10 @@ public class movingRoom : MonoBehaviour
 {
     public Transform t1;
     public GameObject s1;
-    public Transform t2;
-    public GameObject s2;
 
-    public void moveToRoom() 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        s1.transform.position = t1.transform.position;
-    }
-
-    public void moveToCantina()
-    {
-        t2.transform.position = s2.transform.position;
+        s1.transform.position = t1.transform.transform.position;
     }
 
 }
