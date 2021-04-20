0c79253e8167011ff75473b3b33390fca93425cf
Zarozar <nathan25452002@gmail.com>
2021-04-20 22:43:33 +0700

    automaticDoor

---
+++ Assets/Scripts/GAMEPLAY/Cantina/DoorAutomatic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAutomatic : MonoBehaviour
+{
+    [SerializeField] private GameObject doorGameObject;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            CloseDoor();
+        }
+    }
+
+    public void OpenDoor()
+    {
+        animator.SetBool("Open", true);
+    }
+    public void CloseDoor()
+    {
+        animator.SetBool("Open", false);
+    }
+}
