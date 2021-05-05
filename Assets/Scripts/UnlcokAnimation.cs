563ae7f578c6f82d0576e48a6a2aa8a8f83409c8
LAPTOP-TFKNT2RT\Asus <nuttaphoomboonmee@hotmail.com>
2021-05-05 20:10:06 +0700

    -General bug fixes

---
+++ Assets/Scripts/UnlcokAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlcokAnimation : MonoBehaviour
+{
+    public Sprite unlocked_sprite;
+    [SerializeField] private string tag; 
+    private bool unlocked; 
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unlocked_sprite != GetComponent<SpriteRenderer>().sprite)
+        {
+            if (tag == "HomingMissile")
+            {
+                if (DatabaseManager.instance.database.HomingMissile)
+                {
+                    GetComponent<SpriteRenderer>().sprite = unlocked_sprite;
+                }
+            }
+            else if (tag == "Quintuple")
+            {
+                if (DatabaseManager.instance.database.QuintupleGun)
+                {
+                    GetComponent<SpriteRenderer>().sprite = unlocked_sprite; 
+                }
+            }
+        }
+        
+    }
+}
